887a8668f03c5750af0ac3e06609a7df56d82eec
Lagspoon <lagspoon@gmail.com>
2014-04-29 15:45:22 +0200

    gitHub client
    test gitHub client

--- microphone/MIMicrophoneUI.h
+++ microphone/MIMicrophoneUI.h
@@ -5,6 +5,7 @@
 //  Created by Olivier Delecueillerie on 31/03/2014.
 //  Copyright (c) 2014 Olivier Delecueillerie. All rights reserved.
 //
+//
 
 #import <UIKit/UIKit.h>
 #import "FDSoundActivatedRecorder.h"
